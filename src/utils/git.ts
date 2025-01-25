268ed3bf5e346410dc80c22f7a7e948e550bc3e2
Szymon <04_barista_pads@icloud.com>
2025-01-25 22:08:26 +0100

    fix: remove CORS proxy
    We have our own CORS proxy in /cors-proxy edge function.

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -23,8 +23,6 @@ export async function gitClone(repo: GitHubRepository, user: GitHubUser) {
     fs,
     http,
     dir: REPO_DIR,
-    // Use official isomorphic-git CORS proxy
-    corsProxy: "https://cors.isomorphic-git.org",
     url: `https://github.com/${repo.owner}/${repo.name}.git`,
     ref: DEFAULT_BRANCH,
     singleBranch: true,
@@ -79,8 +77,6 @@ export async function gitPull(user: GitHubUser, repo: GitHubRepository) {
     fs,
     http,
     dir: REPO_DIR,
-    // Use official isomorphic-git CORS proxy
-    corsProxy: "https://cors.isomorphic-git.org",
     url: `https://github.com/${repo.owner}/${repo.name}.git`,
     ref: DEFAULT_BRANCH,
     singleBranch: true,
@@ -118,8 +114,6 @@ export async function gitPush(user: GitHubUser, repo: GitHubRepository) {
     fs,
     http,
     dir: REPO_DIR,
-    // Use official isomorphic-git CORS proxy
-    corsProxy: "https://cors.isomorphic-git.org",
     url: `https://github.com/${repo.owner}/${repo.name}.git`,
     ref: DEFAULT_BRANCH,
     onMessage: (message) => console.debug("onMessage", message),
